38dbad084ba58ef2995404cb68b35519329368ff
Maksim Manylov <manylov@gmail.com>
2024-02-14 17:36:22 +0600

    add 1sha256 bench

--- README.md
+++ README.md
@@ -6,4 +6,3 @@ to run specific code, use command like this:
 cargo run --bin sha256
 ```
 
-(look inside ./src/bin folder to see which experiments are available)
---
+++ src/bin/1sha.rs
@@ -0,0 +1,66 @@
+use anyhow::Result;
+use casper_experiments::sha256::make_circuits;
+use plonky2::field::types::Field;
+use plonky2::iop::witness::WitnessWrite;
+use plonky2::iop::witness::{PartialWitness, Witness};
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+use plonky2::plonk::circuit_data::CircuitConfig;
+use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+
+const EXPECTED_RES: [u8; 256] = [
+    0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1,
+    0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1,
+    1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0,
+    1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+    1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1,
+    1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1,
+    0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+];
+
+pub fn array_to_bits(bytes: &[u8]) -> Vec<bool> {
+    let len = bytes.len();
+    let mut ret = Vec::new();
+    for i in 0..len {
+        for j in 0..8 {
+            let b = (bytes[i] >> (7 - j)) & 1;
+            ret.push(b == 1);
+        }
+    }
+    ret
+}
+
+/// An example of using Plonky2 to prove a statement of the form
+/// "I know x² - 4x + 7".
+fn main() -> Result<()> {
+    let mut msg = vec![0; 128 as usize];
+    for i in 0..127 {
+        msg[i] = i as u8;
+    }
+
+    let msg_bits = array_to_bits(&msg);
+    let len = msg.len() * 8;
+    const D: usize = 2;
+    type C = PoseidonGoldilocksConfig;
+    type F = <C as GenericConfig<D>>::F;
+    let mut builder = CircuitBuilder::<F, D>::new(CircuitConfig::standard_recursion_config());
+    let targets = make_circuits(&mut builder, len as u64);
+    let mut pw = PartialWitness::new();
+
+    for i in 0..len {
+        pw.set_bool_target(targets.message[i], msg_bits[i]);
+    }
+
+    for i in 0..EXPECTED_RES.len() {
+        if EXPECTED_RES[i] == 1 {
+            builder.assert_one(targets.digest[i].target);
+        } else {
+            builder.assert_zero(targets.digest[i].target);
+        }
+    }
+
+    let data = builder.build::<C>();
+    let proof = data.prove(pw).unwrap();
+
+    data.verify(proof)
+}
